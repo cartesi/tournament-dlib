a58d54691a24c32ce7a0c5d5032017596e14e089
felipeargento <felipe@cartesi.io>
2019-08-14 19:05:22 +0800

    Remove parameters from instantiate

--- contracts/MatchManagerInterface.sol
+++ contracts/MatchManagerInterface.sol
@@ -16,20 +16,17 @@ contract MatchManagerInterface is Instantiator {
 
     function instantiate(
         uint256 matchRoundDuration,
-        uint256 currentRound,
         uint256 tournamentStartTime,
         uint256 totalPlayers,
         uint256 playersRemaining,
-        address[] unmatchedPlayers,
-        mapping(address => MatchInstantiator.MatchCtx[]) matches,
         address machineAddress) public returns (uint256);
 
     function registerToFirstRound(
         uint256 _index,
         uint256 _finalTime,
-        uint256 score,
-        bytes32 initialHash,
-        bytes32 finalHash
+        uint256 _score,
+        bytes32 _initialHash,
+        bytes32 _finalHash
     ) public;
     function advanceToNextRound(uint256 _index) public;
     function claimWin(uint256 _index) public;
