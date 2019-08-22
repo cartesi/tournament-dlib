41e901f5f33fb2779e2dc86a8aab08945466ecce
felipeargento <felipe@cartesi.io>
2019-08-22 14:03:13 +0800

    Update MatchManager Interface

--- contracts/MatchManagerInterface.sol
+++ contracts/MatchManagerInterface.sol
@@ -12,8 +12,6 @@ contract MatchManagerInterface is Instantiator {
         MatchesOver
     }
 
-    function getCurrentState(uint256 _index, address concernedAddress) public view returns (bytes32);
-
     function instantiate(
         uint256 matchRoundDuration,
         uint256 tournamentStartTime,
@@ -21,16 +19,35 @@ contract MatchManagerInterface is Instantiator {
         uint256 playersRemaining,
         address machineAddress) public returns (uint256);
 
-    function registerToFirstRound(
+    function registerToFirstEpoch(
         uint256 _index,
         uint256 _finalTime,
         uint256 _score,
         bytes32 _initialHash,
         bytes32 _finalHash
     ) public;
+
+    function registerToFirstRound(uint256 _index) public;
+
     function advanceToNextRound(uint256 _index) public;
-    function claimWin(uint256 _index) public;
+
+    function claimWin(uint256 _index) public returns (address);
 
     function isConcerned(uint256 _index, address _user) public view returns (bool);
+    
+    function getState(uint256 _index) public view returns
+        ( uint256 epochDuration,
+          uint256 roundDuration,
+          uint256 currentEpoch,
+          uint256 lastEpochStartTime,
+          uint256 numbersOfMatchesOnLastEpoch,
+          address unmatchedPlayer,
+          uint256 lastMatchIndex,
+          address machineAddress,
+          state currentState
+        );
+
+    function getCurrentState(uint256 _index, address concernedAddress) public view returns (bytes32);
+
 }
 
