3a96ee1695afb5630d6d3c3f9f4dcdbe58da96de
felipeargento <felipe@cartesi.io>
2019-09-20 18:22:18 +0800

    Remove defendHighscore

--- contracts/MatchInterface.sol
+++ contracts/MatchInterface.sol
@@ -13,8 +13,6 @@ contract MatchInterface is Instantiator {
         ClaimerWon
     }
 
-    function getCurrentState(uint256 _index, address concernedAddress) public view returns (bytes32);
-
     function instantiate(
         address _challenger,
         address _claimer,
@@ -27,36 +25,25 @@ contract MatchInterface is Instantiator {
         uint256 _timeOfLastMove) public returns (uint256);
 
     function challengeHighestScore(uint256 _index) public;
-
-    function defendHighestScore(uint256 _index) public;
-
     function winByVG(uint256 _index) public;
-
     function claimVictoryByTime(uint256 _index) public;
-
     function stateIsFinishedClaimerWon(uint256 _index) public view returns (bool);
-
     function stateIsFinishedChallengerWon(uint256 _index) public view returns (bool);
-
     function getEpochNumber(uint256 _index) public view returns (uint256);
-
     function isClaimer(uint256 _index, address addr) public view returns (bool);
-
     function isChallenger(uint256 _index, address addr) public view returns (bool);
-
     function isConcerned(uint256 _index, address _user) public view returns (bool);
-
-    function getState(uint256 _index, address _user) public view returns
-    (   address _challenger,
-        address _claimer,
-        uint256 _epochNumber,
-        uint256 _roundDuration,
-        uint256 _timeOfLastMove,
-        address _machine,
-        bytes32 _initialHash,
-        bytes32 _finalHash,
-        uint256 _finalTime,
-        state _currentState
+    function getState(uint256 _index, address) public view returns
+    (   address challenger,
+        address claimer,
+        uint256 epochNumber,
+        uint256 roundDuration,
+        uint256 timeOfLastMove,
+        address machine,
+        bytes32 initialHash,
+        bytes32 finalHash,
+        uint256 finalTime,
+        bytes32 currentState
    );
 
 }
