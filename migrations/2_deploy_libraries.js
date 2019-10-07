02dd82ff5953547ea17d65743c6641c891841411
felipeargento <felipe@cartesi.io>
2019-10-07 14:23:05 +0800

    Fix migration script

--- migrations/2_deploy_libraries.js
+++ migrations/2_deploy_libraries.js
@@ -5,25 +5,26 @@ var RevealMock = artifacts.require("./RevealMock.sol");
 var DAppMock = artifacts.require("./DAppMock.sol");
 
 module.exports = function(deployer, network, accounts) {
-    deployer.deploy(VGMock).then(function() {
-        deployer.deploy(MatchInstantiator, VGMock.address).then(function(){
-            deployer.deploy(MatchManagerInstantiator, MatchInstantiator.address)
-        });
+
+    deployer.then(async () => {
+        await deployer.deploy(VGMock);
+        await deployer.deploy(MatchInstantiator, VGMock.address);
+        await deployer.deploy(MatchManagerInstantiator, MatchInstantiator.address)
+
+        // add main "player" values here before adding other accounts
+        var playerAddresses = [accounts[0]];
+        var scores = [100];
+        var finalHashes = ["0x01"];
+
+        for (var i = 1; i < 9; i++) {
+            playerAddresses.push(accounts[i]);
+            scores.push(i * 20);
+            finalHashes.push("0x00");
+        }
+
+        await deployer.deploy(RevealMock, MatchManagerInstantiator.address);
+        await deployer.deploy(DAppMock, RevealMock.address, playerAddresses, scores, finalHashes);
     });
 
-    // add main "player" values here before adding other accounts
-    var playerAddresses = [accounts[0]];
-    var scores = [100];
-    var finalHashes = ["0x01"];
-
-    for (var i = 1; i < 9; i++) {
-        playerAddresses.push(accounts[i]);
-        scores.push(i * 20);
-        finalHashes.push("0x00");
-    }
-
-    deployer.deploy(RevealMock).then(function() {
-        deployer.deploy(DAppMock, RevealMock.address, playerAddresses, scores, finalHashes);
-    });
 };
 
