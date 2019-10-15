9eefa24e619f5d87ebf107e5e8ed399bdfed7737
Stephen <stephen@cartesi.io>
2019-10-15 20:11:55 +0800

    Add docker cp to export wallets.yaml to host

--- build_tournament_blockchain_base_image.sh
+++ build_tournament_blockchain_base_image.sh
@@ -1,1 +1,7 @@
 docker build -t cartesi/image-tournament-blockchain-base .
+
+rm -rf blockchain_files
+mkdir -p blockchain_files
+id=$(docker create cartesi/image-tournament-blockchain-base)
+docker cp $id:/opt/cartesi/blockchain_files/wallets.yaml blockchain_files/wallets.yaml
+docker rm -v $id
