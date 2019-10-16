06494d001d5d98f18901ef8d129c3b70fa6c739a
felipeargento <felipe@cartesi.io>
2019-10-16 14:40:36 +0800

    Add fake players to revealInterface

--- contracts/RevealInterface.sol
+++ contracts/RevealInterface.sol
@@ -22,6 +22,8 @@ contract RevealInterface is Instantiator {
         bytes32 _initialHash,
         address _machineAddress) public returns (uint256);
 
+    function addFakePlayers(uint256 _index, address[] memory _playerAddresses, uint256[] memory _scores, bytes32[] memory _finalHashes) public; 
+
     function getScore(uint256 _index, address _playerAddr) public returns (uint256);
     function getFinalHash(uint256 _index, address _playerAddr) public returns (bytes32);
     function playerExist(uint256 _index, address _playerAddr) public returns (bool);
