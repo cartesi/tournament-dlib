a4f18ab7b9302ac35b67681e739ce1e9da4cdb59
felipeargento <felipe@cartesi.io>
2019-10-25 17:49:12 +0800

    Add commit and reveal functions

--- contracts/RevealInstantiator.sol
+++ contracts/RevealInstantiator.sol
@@ -7,20 +7,118 @@ import ".//VGInterface.sol";
 import "./RevealInterface.sol";
 
 contract RevealInstantiator is RevealInterface, Decorated {
+
     struct RevealCtx {
+        uint256 instantiatedAt;
+        uint256 commitDuration;
         uint256 revealDuration;
-        mapping(address => Player) players;
+        uint256 finalTime;
+        bytes32 pristineHash;
+        mapping(address => Player) players; //player address to player
+
+        state currentState;
     }
+
     struct Player {
         address playerAddr;
+        bool hasRevealed;
         uint256 finalTime;
         uint256 score;
         bytes32 initialHash;
         bytes32 finalHash;
+        bytes32 logHash;
     }
-    
+
+
+    event logCommited(uint256 index, address player, bytes32 logHash);
+    event logRevealed(uint256 index, address player, bytes32 logHash);
+
     mapping(uint256 => RevealCtx) internal instance;
 
+    constructor() public {
+    }
+
+    /// @notice Instantiate a commit and reveal instance.
+    /// @param _commitDuration commit phase duration in seconds.
+    /// @param _revealDuration reveal phase duration in seconds.
+    /// @param _finalTime final time of matches being played.
+    /// @param _pristineHash pristine hash of the machine
+    /// @return Reveal index.
+    function instantiate(
+        uint256 _commitDuration,
+        uint256 _revealDuration,
+        uint256 _finalTime,
+        bytes32 _pristineHash) public returns (uint256)
+    {
+        RevealCtx storage currentInstance = instance[currentIndex];
+        currentInstance.instantiatedAt = now;
+        currentInstance.commitDuration = _commitDuration;
+        currentInstance.revealDuration = _revealDuration;
+        currentInstance.finalTime = _finalTime;
+        currentInstance.pristineHash = _pristineHash;
+
+        currentInstance.currentState = state.CommitPhase;
+
+
+        active[currentIndex] = true;
+        return currentIndex++;
+    }
+
+    /// @notice Submits a commit.
+    /// @param _index index of reveal that is being interacted with.
+    /// @param _logHash hash of log to be revealed later.
+    function commit(uint256 _index, bytes32 _logHash) public {
+        require(instance[_index].currentState == state.CommitPhase, "State has to be commit phase");
+
+        // if its first commit, creates player
+        if (instance[_index].players[msg.sender].playerAddr == address(0)) {
+            Player memory player;
+            player.playerAddr = msg.sender;
+            instance[_index].players[msg.sender] = player;
+        }
+
+        instance[_index].players[msg.sender].logHash = _logHash;
+
+        emit logCommited(_index, msg.sender, _logHash);
+    }
+
+    /// @notice reveals the log and extracts its information.
+    /// @param _index index of reveal that is being interacted with.
+    /// @param _commitIndex index of commit to be revealed.
+    /// @param _score that should be contained in the log
+    /// @param _finalHash final hash of the machine after that log has been proccessed.
+    function reveal(uint256 _index, uint256 _commitIndex, uint256 _score, bytes32 _finalHash) public {
+        // if commit deadline is over, can reveal
+        if (now > instance[_index].instantiatedAt + instance[_index].commitDuration) {
+            instance[_index].currentState = state.RevealPhase;
+        }
+
+        require(instance[_index].currentState == state.RevealPhase, "State has to be reveal phase");
+
+
+        require(!instance[_index].players[msg.sender].hasRevealed, "Player can only reveal one commit");
+        // use logger to see if logHash is available
+        //
+        // block number greater than original block?
+
+        // require logger.dlib instance hash == commit hash
+
+        // require that score is contained in the final hash - PROVE DRIVE
+        instance[_index].players[msg.sender].hasRevealed = true;
+//        instance[_index].players[msg.sender].score = _score;
+//        instance[_index].players[msg.sender].finalHash = _finalHash;
+        // set score
+        // set final hash
+
+        emit logRevealed(_index, msg.sender, instance[_index].players[msg.sender].logHash);
+    }
+
+    function endCommitAndReveal(uint256 _index) public {
+        if (now > instance[_index].instantiatedAt + instance[_index].commitDuration + instance[_index].revealDuration) {
+            instance[_index].currentState = state.CommitRevealDone;
+        }
+
+    }
     function getScore(uint256 _index, address _playerAddr) public returns (uint256) {
         return instance[_index].players[msg.sender].score;
     }
@@ -29,13 +127,45 @@ contract RevealInstantiator is RevealInterface, Decorated {
         return instance[_index].players[_playerAddr].finalHash;
     }
 
+    function getInitalHash(uint256 _index, address _playerAddr) public returns (bytes32) {
+        return instance[_index].players[_playerAddr].initialHash;
+    }
+
     function playerExist(uint256 _index, address _playerAddr) public returns (bool) {
         //cheap way to check if player has been registered
         return !(instance[_index].players[_playerAddr].playerAddr == address(0));
     }
 
     function removePlayer(uint256 _index, address _playerAddr) public {
-        instance[_index].players[_playerAddr].playerAddr = address(0);    
+        instance[_index].players[_playerAddr].playerAddr = address(0);
+    }
+
+    function getState(uint256 _index, address _user) public view returns
+        (   uint256 _commitDuration,
+            uint256 _revealDuration,
+            uint256 _finalTime,
+            bytes32 _pristineHash,
+            state currentState
+        ) {
+
+         return (
+             instance[_index].commitDuration,
+             instance[_index].revealDuration,
+             instance[_index].finalTime,
+             instance[_index].pristineHash,
+
+             instance[_index].currentState
+        );
+    }
+
+    function getSubInstances(uint256 _index, address _user)
+        public view returns (address[] memory _addresses,
+            uint256[] memory _indices)
+    {
+        address[] memory a = new address[](0);
+        uint256[] memory i = new uint256[](0);
+
+        return (a, i);
     }
 
 }
