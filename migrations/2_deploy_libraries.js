7a8e0c828e34242dfbf4defb7ca6bf9faac1274f
felipeargento <felipe@cartesi.io>
2019-10-27 19:10:40 +0800

    Add RevealInstantiator to deploy script

--- migrations/2_deploy_libraries.js
+++ migrations/2_deploy_libraries.js
@@ -1,6 +1,8 @@
-var VGMock = artifacts.require("./VGMock.sol");
-var MatchInstantiator = artifacts.require("./MatchInstantiator.sol");
+var RevealInstantiator = artifacts.require("./RevealInstantiator.sol");
 var MatchManagerInstantiator = artifacts.require("./MatchManagerInstantiator.sol");
+var MatchInstantiator = artifacts.require("./MatchInstantiator.sol");
+
+var VGMock = artifacts.require("./VGMock.sol");
 var RevealMock = artifacts.require("./RevealMock.sol");
 var DAppMock = artifacts.require("./DAppMock.sol");
 
@@ -9,7 +11,8 @@ module.exports = function(deployer, network, accounts) {
     deployer.then(async () => {
         await deployer.deploy(VGMock);
         await deployer.deploy(MatchInstantiator, VGMock.address);
-        await deployer.deploy(MatchManagerInstantiator, MatchInstantiator.address)
+        await deployer.deploy(MatchManagerInstantiator, MatchInstantiator.address);
+        await deployer.deploy(RevealInstantiator);
 
         // add main "player" values here before adding other accounts
         var playerAddresses = [accounts[0]];
