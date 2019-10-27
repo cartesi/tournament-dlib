94d421370f376f7679c8bb7b7535d6fc2a501910
felipeargento <felipe@cartesi.io>
2019-10-27 19:09:57 +0800

    Update Reveal interface

--- contracts/RevealInterface.sol
+++ contracts/RevealInterface.sol
@@ -2,7 +2,7 @@
 /// @author Felipe Argento
 pragma solidity ^0.5.0;
 
-import ".//Instantiator.sol";
+import "./Instantiator.sol";
 
 contract RevealInterface is Instantiator {
 
@@ -17,15 +17,37 @@ contract RevealInterface is Instantiator {
         uint256 _revealDuration,
         uint256 _finalTime,
         bytes32 _pristineHash,
-        uint64 _scoreDrivePosition,
-        uint64 _logDrivePosition,
-        uint64 _scoreDriveLogSize,
-        uint64 _logDriveLogSize) public returns (uint256);
+        uint256 _scoreDrivePosition,
+        uint256 _logDrivePosition,
+        uint256 _scoreDriveLogSize,
+        uint256 _logDriveLogSize) public returns (uint256);
 
     function getScore(uint256 _index, address _playerAddr) public returns (uint256);
-    function getInitalHash(uint256 _index, address _playerAddr) public returns (bytes32);
+
+    function getInitialHash(uint256 _index, address _playerAddr) public returns (bytes32);
+
     function getFinalHash(uint256 _index, address _playerAddr) public returns (bytes32);
+
     function playerExist(uint256 _index, address _playerAddr) public returns (bool);
-    function getCurrentState(uint256 _index, address concernedAddress) public view returns (bytes32);
+
+    function isConcerned(uint256 _index, address _user) public view returns (bool);
+
+    function getCurrentState(uint256 _index) public view returns (bytes32);
+
+    function getState(uint256 _index, address)
+    public view returns (
+         uint256 instantiatedAt,
+         uint256 commitDuration,
+         uint256 revealDuration,
+         uint256 finalTime,
+         bytes32 pristineHash,
+         uint256 scoreDrivePosition,
+         uint256 logDrivePosition,
+         uint256 scoreDriveLogSize,
+         uint256 logDriveLogSize,
+         bool hasRevealed,
+
+         bytes32 currentState
+    );
 
 }
