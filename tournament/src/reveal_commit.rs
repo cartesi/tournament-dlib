e79ebd2e0ce4abfc905fb4df15e86a439f07f4fb
felipeargento <felipe@cartesi.io>
2019-11-12 15:06:35 +0800

    Add prepare_reveal function

--- tournament/src/reveal_commit.rs
+++ tournament/src/reveal_commit.rs
@@ -56,6 +56,12 @@ pub struct RevealCommitCtx {
     pub current_state: String,
 }
 
+#[derive(Default)]
+pub struct MachineTemplate {
+    pub machine: cartesi_base::MachineRequest,
+    pub drive_path: String
+}
+
 #[derive(Deserialize, Debug)]
 struct Payload {
     action: String,
@@ -89,7 +95,7 @@ impl From<RevealCommitCtxParsed> for RevealCommitCtx {
     }
 }
 
-impl DApp<()> for RevealCommit {
+impl DApp<(MachineTemplate)> for RevealCommit {
     /// React to the Reveal contract
     fn react(
         instance: &state::Instance,
@@ -153,23 +159,13 @@ impl DApp<()> for RevealCommit {
 
                         if phase_is_over {
                             // if commit phase is over, player reveal his log and forces the phase change
-                            // TO-DO: complete transaction parameters
-                            let request = TransactionRequest {
-                                concern: instance.concern.clone(),
-                                value: U256::from(0),
-                                function: "reveal".into(),
-                                data: vec![
-                                    Token::Uint(instance.index),
-                                    //Token::Uint(score),
-                                    //Token::FixedBytes(finalHash),
-                                    //Token::FixedBytes(logDriveHash),
-                                    //Token::FixedBytes(scoreDriveHash),
-                                    //Token::Array(logDriveSiblings),
-                                    //Token::Array(scoreDriveSiblings)
-                                ],
-                                strategy: transaction::Strategy::Simplest,
-                            };
-                            return Ok(Reaction::Transaction(request));
+                            return prepare_reveal_transaction(
+                                &instance.concern,
+                                instance.index,
+                                archive,
+                                ctx.log_hash,
+                                machine_template,
+                            );
                         }
                         // If there is no post and the phase is not over, idles
                         return Ok(Reaction::Idle);
@@ -225,97 +221,15 @@ impl DApp<()> for RevealCommit {
                     .into();
                 trace!("Submitted! Result: {:?}...", processed_response.hash);
 
-                // get score from emulator
-                // TODO: fill in the time, address and length parameters
-                let id = build_machine_id(
-                    instance.index,
-                    &instance.concern.contract_address,
-                );
-                let id_clone = id.clone();
-                let time = 0;
-                let address = 0;
-                let length = 0;
-
-                let archive_key = build_session_read_key(id.clone(), time, address, length);
-                let mut position = cartesi_base::ReadMemoryRequest::new();
-                position.set_address(address);
-                position.set_length(length);
-
-                let request = SessionReadMemoryRequest {
-                    session_id: id.clone(),
-                    time: time,
-                    position: position
-                };
-
-                let processed_response: SessionReadMemoryResult = archive.get_response(
-                    EMULATOR_SERVICE_NAME.to_string(),
-                    archive_key.clone(),
-                    EMULATOR_METHOD_READ.to_string(),
-                    request.into())?
-                    .map_err(move |_e| {
-                        Error::from(ErrorKind::ArchiveInvalidError(
-                            EMULATOR_SERVICE_NAME.to_string(),
-                            id_clone,
-                            EMULATOR_METHOD_READ.to_string()))
-                    })?
-                    .into();
-
-                trace!("Read memory result: {:?}...", processed_response.read_content.data);
-
-                // get hash of log drive from emulator
-                // TODO: fill in the time, address and log2_size parameters
-                let id_clone = id.clone();
-                let time = 0;
-                let address = 0;
-                let log2_size = 0;
-
-                let archive_key = build_session_proof_key(id.clone(), time, address, log2_size);
-                let mut target = cartesi_base::GetProofRequest::new();
-                target.set_address(address);
-                target.set_log2_size(log2_size);
-
-                let request = SessionGetProofRequest {
-                    session_id: id.clone(),
-                    time: time,
-                    target: target
-                };
-
-                let processed_response: SessionGetProofResult = archive.get_response(
-                    EMULATOR_SERVICE_NAME.to_string(),
-                    archive_key.clone(),
-                    EMULATOR_METHOD_PROOF.to_string(),
-                    request.into())?
-                    .map_err(move |_e| {
-                        Error::from(ErrorKind::ArchiveInvalidError(
-                            EMULATOR_SERVICE_NAME.to_string(),
-                            id_clone,
-                            EMULATOR_METHOD_PROOF.to_string()))
-                    })?
-                    .into();
-
-                trace!("Get proof result: {:?}...", processed_response.proof);
 
                 // else complete reveal
-                // TO-DO: complete transaction parameters
-                let request = TransactionRequest {
-                    concern: instance.concern.clone(),
-                    value: U256::from(0),
-                    function: "reveal".into(),
-                    data: vec![
-                        Token::Uint(instance.index),
-                        //Token::Uint(score),
-                        //Token::FixedBytes(finalHash),
-                        //Token::FixedBytes(logDriveHash),
-                        //Token::FixedBytes(scoreDriveHash),
-                        //Token::Array(logDriveSiblings),
-                        //Token::Array(scoreDriveSiblings)
-                    ],
-                    strategy: transaction::Strategy::Simplest,
-                };
-
-                return Ok(Reaction::Transaction(request));
-
-
+                return prepare_reveal_transaction(
+                    &instance.concern,
+                    instance.index,
+                    archive,
+                    ctx.log_hash,
+                    machine_template,
+                );
             }
 
             _ => {
@@ -327,7 +241,7 @@ impl DApp<()> for RevealCommit {
     fn get_pretty_instance(
         instance: &state::Instance,
         archive: &Archive,
-        _: &(),
+        _: &MachineTemplate,
     ) -> Result<state::Instance> {
 
         // get context (state) of the match instance
@@ -355,3 +269,214 @@ impl DApp<()> for RevealCommit {
     }
 }
 
+pub fn prepare_reveal_transaction(
+    concern: &Concern,
+    index: U256,
+    archive: &Archive,
+    log_hash: H256,
+    machine_template: &MachineTemplate,
+
+) -> Result<Reaction> {
+    // TO-DO: Define final_time!
+    let final_time = 500;
+
+    let path = format!("{:x}.log", log_hash);
+    let file_path = FilePath {
+        path: path.clone()
+    };
+
+    // get score from emulator
+    let id = build_machine_id(
+        index,
+        &concern.contract_address,
+    );
+
+    // Score is the first word (logsize = 3) of the output drive
+    // The output drive starts at address: (1<<63)+(3<<61)
+    // The score is there when the machine halts (final_time)
+    let id_clone = id.clone();
+    let time = final_time;
+    let address = (1<<63)+(3<<61);
+
+    // TO-DO: is log2size and length the same thing?
+    let length = 3;
+
+    let archive_key = build_session_read_key(id.clone(), time, address, length);
+    let mut position = cartesi_base::ReadMemoryRequest::new();
+    position.set_address(address);
+    position.set_length(length);
+
+    let request = SessionReadMemoryRequest {
+        session_id: id.clone(),
+        time: time,
+        position: position
+    };
+
+    let processed_response: SessionReadMemoryResult = archive.get_response(
+        EMULATOR_SERVICE_NAME.to_string(),
+        archive_key.clone(),
+        EMULATOR_METHOD_READ.to_string(),
+        request.into())?
+        .map_err(move |_e| {
+            Error::from(ErrorKind::ArchiveInvalidError(
+                EMULATOR_SERVICE_NAME.to_string(),
+                id_clone,
+                EMULATOR_METHOD_READ.to_string()))
+        })?
+        .into();
+
+    trace!("Read memory result: {:?}...", processed_response.read_content.data);
+
+    let score = processed_response.read_content.data;
+
+    // get score proof (siblings)
+    let id_clone = id.clone();
+
+    let archive_key = build_session_proof_key(id.clone(), time, address, length);
+    let mut target = cartesi_base::GetProofRequest::new();
+    target.set_address(address);
+    target.set_log2_size(length);
+
+    let request = SessionGetProofRequest {
+        session_id: id.clone(),
+        time: time,
+        target: target
+    };
+
+    let processed_response: SessionGetProofResult = archive.get_response(
+        EMULATOR_SERVICE_NAME.to_string(),
+        archive_key.clone(),
+        EMULATOR_METHOD_PROOF.to_string(),
+        request.into())?
+        .map_err(move |_e| {
+            Error::from(ErrorKind::ArchiveInvalidError(
+                EMULATOR_SERVICE_NAME.to_string(),
+                id_clone,
+                EMULATOR_METHOD_PROOF.to_string()))
+        })?
+        .into();
+
+    trace!("Get proof result: {:?}...", processed_response.proof);
+
+    let score_siblings = processed_response.proof;
+
+    // get hash of log drive from emulator
+    // Log drive starts at address (1<<63)+(2<<61)
+    // Log size is 1 MB
+    // Siblings should be checked against template hash (time = 0)
+    let id_clone = id.clone();
+    let time = 0;
+    let address = (1<<63)+(2<<61);
+    let log2_size = 20; // 1MB
+
+    let archive_key = build_session_proof_key(id.clone(), time, address, log2_size);
+    let mut target = cartesi_base::GetProofRequest::new();
+    target.set_address(address);
+    target.set_log2_size(log2_size);
+
+    let request = SessionGetProofRequest {
+        session_id: id.clone(),
+        time: time,
+        target: target
+    };
+
+    let processed_response: SessionGetProofResult = archive.get_response(
+        EMULATOR_SERVICE_NAME.to_string(),
+        archive_key.clone(),
+        EMULATOR_METHOD_PROOF.to_string(),
+        request.into())?
+        .map_err(move |_e| {
+            Error::from(ErrorKind::ArchiveInvalidError(
+                EMULATOR_SERVICE_NAME.to_string(),
+                id_clone,
+                EMULATOR_METHOD_PROOF.to_string()))
+        })?
+        .into();
+
+    trace!("Get proof result: {:?}...", processed_response.proof);
+
+    let log_siblings = processed_response.proof;
+
+    // build machine
+    let id = build_machine_id(
+        index,
+        &concern.contract_address,
+    );
+
+    // add log drive to template machine
+    fs::copy(log_file.clone(), machine_template.drive_path.clone())?;
+
+    let request = NewSessionRequest {
+        session_id: id.clone(),
+        machine: machine_template.machine.clone()
+    };
+
+    // send newSession request to the emulator service
+    let id_clone = id.clone();
+    let duplicate_session_msg = format!("Trying to register a session with a session_id that already exists: {}", id);
+    let _processed_response: NewSessionResult = archive.get_response(
+        EMULATOR_SERVICE_NAME.to_string(),
+        id.clone(),
+        EMULATOR_METHOD_NEW.to_string(),
+        request.into())?
+        .map_err(move |e| {
+            if e == duplicate_session_msg {
+                Error::from(ErrorKind::ArchiveNeedsDummy(
+                    EMULATOR_SERVICE_NAME.to_string(),
+                    id_clone,
+                    EMULATOR_METHOD_NEW.to_string()))
+            } else {
+                Error::from(ErrorKind::ArchiveInvalidError(
+                    EMULATOR_SERVICE_NAME.to_string(),
+                    id_clone,
+                    EMULATOR_METHOD_NEW.to_string()))
+            }
+        })?
+        .into();
+
+    // TO-DO: what is final time?
+    let sample_points: Vec<u64> =
+        vec![0, final_time];
+
+    let request = SessionRunRequest {
+        session_id: id.clone(),
+        times: sample_points.clone(),
+    };
+    let archive_key = build_session_run_key(
+        id.clone(),
+        sample_points.clone());
+    let id_clone = id.clone();
+
+    trace!("Calculating final hash of machine {}", id);
+    // have we sampled the final time?
+    let processed_response: SessionRunResult = archive.get_response(
+        EMULATOR_SERVICE_NAME.to_string(),
+        archive_key.clone(),
+        EMULATOR_METHOD_RUN.to_string(),
+        request.into())?
+        .map_err(move |_e| {
+            Error::from(ErrorKind::ArchiveInvalidError(
+                EMULATOR_SERVICE_NAME.to_string(),
+                id_clone,
+                EMULATOR_METHOD_RUN.to_string()))
+        })?
+        .into();
+
+        let final_hash = processed_response.hashes[1];
+
+        let request = TransactionRequest {
+            concern: concern.clone(),
+            value: U256::from(0),
+            function: "reveal".into(),
+            data: vec![
+                Token::Uint(index),
+                Token::Uint(score),
+                Token::FixedBytes(final_hash),
+                Token::Array(log_siblings),
+                Token::Array(score_siblings)
+            ],
+            strategy: transaction::Strategy::Simplest,
+        };
+
+        return Ok(Reaction::Transaction(request));
+}
