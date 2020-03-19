4ed53dd66fcaf147209e9cba93719aca3f385051
felipeargento <felipe@cartesi.io>
2020-03-19 23:16:27 +0800

    Remove Bitsmanipulation from deployer

--- migrations/2_deploy_libraries.js
+++ migrations/2_deploy_libraries.js
@@ -1,5 +1,4 @@
 const contract = require("@truffle/contract");
-const BitsManipulationLibrary = contract(require("@cartesi/util/build/contracts/BitsManipulationLibrary.json"));
 const VGInstantiator = contract(require("@cartesi/arbitration/build/contracts/VGInstantiator.json"));
 const Logger = contract(require("@cartesi/logger/build/contracts/Logger.json"));
 
@@ -10,11 +9,9 @@ const MatchInstantiator = artifacts.require("MatchInstantiator");
 module.exports = function(deployer) {
 
     deployer.then(async () => {
-        BitsManipulationLibrary.setNetwork(deployer.network_id);
         VGInstantiator.setNetwork(deployer.network_id);
         Logger.setNetwork(deployer.network_id);
 
-        await deployer.link(BitsManipulationLibrary, RevealInstantiator);
         await deployer.deploy(MatchInstantiator, VGInstantiator.address);
         await deployer.deploy(MatchManagerInstantiator, MatchInstantiator.address);
         await deployer.deploy(RevealInstantiator, Logger.address);
